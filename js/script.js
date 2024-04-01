f35ca969cbd8e74eee468af083333f4cc43f4a11
ThomasAidesNsoft <thomasbages@outlook.fr>
2024-04-01 18:29:42 +0200

    ajout des switchs des weather-icons

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ const UrlApi="https://api.openweathermap.org/data/2.5/weather?q="
 
 const searchBox = document.querySelector(".search input");
 const  searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city){
 
     // on attend l'objet response renvoyé par la méthode fecth
     const response = await fetch( UrlApi + city + "&appid=" + ApiKey);
-    alert(response);
     // L'objet Response contient diverses méthodes permettant d'extraire les données de la réponse,
     // Ces méthodes renvoient également des promesses, car elles peuvent impliquer des opérations asynchrones, telles que le téléchargement et le traitement du corps de la réponse.
     var data = await response.json();
@@ -21,11 +21,27 @@ async function checkWeather(city){
     document.querySelector(".temp").innerHTML = math.round(data.main.temp)+" °C";
     document.querySelector(".humidity").innerHTML = data.main.humidity +" %";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    console.log(data.weather.main);
+
+    if(data.weather[0].main === "Clouds"){
+        weatherIcon.src = "img/clouds.png";
+    } else if(data.weather[0].main === "Clear"){
+        weatherIcon.src = "img/clear.png";
+    }else if(data.weather[0].main === "Rain"){
+        weatherIcon.src = "img/rain.png";
+    }else if(data.weather[0].main === "Drizzle"){
+        weatherIcon.src = "img/drizzle.png";
+    }else if(data.weather[0].main === "Mist"){
+        weatherIcon.src = "img/mist.png";
+    }
+
+
+    document.querySelector("weather").computedStyleMap.display = "block";
+  
 }
 
 searchBtn.addEventListener("click",() => {
     checkWeather(searchBox.value);
-    
 })
 
 searchBtn.addEventListener('keypress',function(event){
