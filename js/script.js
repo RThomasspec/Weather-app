b423b7c0e99f003414689784e16b82dc6302fb16
ThomasAidesNsoft <thomasbages@outlook.fr>
2025-03-22 12:33:30 +0100

    modif js queryselector

--- js/script.js
+++ js/script.js
@@ -1,58 +1,60 @@
-const ApiKey= "caf06e143a0f7d0f052c84045cfdee20";
-const UrlApi="https://api.openweathermap.org/data/2.5/weather?q="
+const ApiKey = "caf06e143a0f7d0f052c84045cfdee20";
+const UrlApi = "https://api.openweathermap.org/data/2.5/weather?q="
 
 const searchBox = document.querySelector(".search input");
-const  searchBtn = document.querySelector(".search button");
+const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
-async function checkWeather(city){
+async function checkWeather(city) {
 
- 
+
     // on attend l'objet response renvoyé par la méthode fecth
-    const response = await fetch( UrlApi + city + "&appid=" + ApiKey);
+    const response = await fetch(UrlApi + city + "&appid=" + ApiKey);
 
-    if(response.status == 404){
+    // tres imporatnt sinon o nrique de crée un objet JSON avec une errreur avaec le response.json();
+    if (response.status == 404) {
         document.querySelector(".weather").style.display = "none";
         document.querySelector(".error").style.display = "block";
     }
-    else{
+    else {
         document.querySelector(".error").style.display = "none";
-    // L'objet Response contient diverses méthodes permettant d'extraire les données de la réponse,
-    // Ces méthodes renvoient également des promesses, car elles peuvent impliquer des opérations asynchrones, telles que le téléchargement et le traitement du corps de la réponse.
-    var data = await response.json();
+        // L'objet Response contient diverses méthodes permettant d'extraire les données de la réponse,
+        // Ces méthodes renvoient également des promesses, car elles peuvent impliquer des opérations asynchrones, 
+        // tel que le téléchargement et le traitement du corps de la réponse.
+        var data = await response.json();
 
-   
-    
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+        console.log(data.weather.main);
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+" °C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity +" %";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-    console.log(data.weather.main);
+        if (data.weather[0].main === "Clouds") {
+            weatherIcon.src = "img/clouds.png";
+        } else if (data.weather[0].main === "Clear") {
+            weatherIcon.src = "img/clear.png";
+        } else if (data.weather[0].main === "Rain") {
+            weatherIcon.src = "img/rain.png";
+        } else if (data.weather[0].main === "Drizzle") {
+            weatherIcon.src = "img/drizzle.png";
+        } else if (data.weather[0].main === "Mist") {
+            weatherIcon.src = "img/mist.png";
+        }
 
-    if(data.weather[0].main === "Clouds"){
-        weatherIcon.src = "img/clouds.png";
-    } else if(data.weather[0].main === "Clear"){
-        weatherIcon.src = "img/clear.png";
-    }else if(data.weather[0].main === "Rain"){
-        weatherIcon.src = "img/rain.png";
-    }else if(data.weather[0].main === "Drizzle"){
-        weatherIcon.src = "img/drizzle.png";
-    }else if(data.weather[0].main === "Mist"){
-        weatherIcon.src = "img/mist.png";
+
+        document.querySelector(".weather").style.display = "block";
     }
-
-
-    document.querySelector(".weather").style.display = "block";
 }
-}
+
 document.querySelector(".error").style.display = "none";
-searchBtn.addEventListener("click",() => {
+
+
+searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
 })
 
-searchBtn.addEventListener('keypress',function(event){
-    if(event.key === 'Enter'){
+searchBtn.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
         checkWeather(searchBox.value);
     }
 })
